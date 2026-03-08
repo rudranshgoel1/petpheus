f882984357d37ad954d54017439c6fc30569b953
rudranshgoel1 <app.whatsupleads@gmail.com>
2026-03-09 01:06:35 +0530

    added the api thingy

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from slackeventsapi import SlackEventAdapter
 import hashlib
 import hmac
 import time
+import requests
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
@@ -26,13 +27,16 @@ def verify_slack_signature(req):
         return False
     
     sig_basestring = f"v0:{timestamp}:{req.get_data(as_text=True)}"
-    my_signature = 'v0=' + hmac.new(slack_signing_secret, sig_basestring.encode(), hashlib.sha256).hexidigest()
+    my_signature = 'v0=' + hmac.new(slack_signing_secret, sig_basestring.encode(), hashlib.sha256).hexdigest()
     slack_signature = req.headers.get('X-Slack-Signature', '')
 
     return hmac.compare_digest(my_signature, slack_signature)
 
 @app.route('/slack/events', methods=['POST'])
 def slack_events():
+    if not verify_slack_signature(request):
+        return "invalid signature", 403
+
     data = request.json
 
     if data.get('type') == 'url_verification':
@@ -46,18 +50,33 @@ def slack_events():
 
 @slack_event_adapter.on('message')
 def message(payload):
+    print(payload)
     event = payload.get('event', {})
     channel_id = event.get('channel')
     user_id = event.get('user')
-    text = event.get('text')
+    text = event.get('text', '')
+    files = event.get('files', [])
 
     if BOT_ID != user_id:
         if '##' in text:
             return
-        elif text: 
-            ts = event.get('ts')
-            client.reactions_add(channel=channel_id, name='loading', timestamp=ts)
-            client.chat_postMessage(channel=channel_id, thread_ts=ts, text="making emoji... (not rn it is still in development)")
+        elif files: 
+            file = files[0]
+
+            if file['mimetype'].startswith('image'):
+                image_url = file['url_private']
+
+                ts = event.get('ts')
+                client.reactions_add(channel=channel_id, name='loading', timestamp=ts)
+                client.chat_postMessage(channel=channel_id, thread_ts=ts, text="making emoji... (not rn it is still in development)")
+
+
+                r = requests.get("https://patpatgifmaker.vercel.app/api/petpet", params={
+                    "image_url": image_url,
+                    "slack_token": os.environ["SLACK_TOKEN"],
+                })
+                gif_url = r.json()["gif_url"]
+
         else:
             return
 
